d3ff5d4c9c9ff589d059866cbe2a582d33f33936
gugasant <gustavosantlopes3@gmail.com>
2024-03-06 21:26:03 -0300

    commit inicial

---
+++ client/src/constants/index.ts
@@ -0,0 +1,27 @@
+export const propertyReferralsInfo = [
+  {
+    title: 'Tecnologia',
+    percentage: 64,
+    color: '#6C5DD3',
+  },
+  {
+    title: 'Comércio',
+    percentage: 40,
+    color: '#7FBA7A',
+  },
+  {
+    title: 'Construção Civil',
+    percentage: 50,
+    color: '#FFCE73',
+  },
+  {
+    title: 'Empreendedorismo',
+    percentage: 80,
+    color: '#FFA2C0',
+  },
+  {
+    title: 'Outros',
+    percentage: 15,
+    color: '#F45252',
+  },
+];
